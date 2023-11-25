aeee996898ee980eb1d0d612bcbed1f42ac19f04
tanersaydam <tanersaydam@gmail.com>
2023-11-25 07:13:16 +0300

    first commit

---
+++ HelloWorld/HelloWorld.Send/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace HelloWorld.Send;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        factory.Port = 5672;
+        factory.UserName = "guest";
+        factory.Password = "guest";
+
+        //factory.Uri = new Uri("amqps://qswqssku:5V-JhWitvvVXTfbVfm_d3yUvMIIaS47P@rat.rmq2.cloudamqp.com/qswqssku");
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+           );
+
+        string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: "hello",
+            basicProperties: null,
+            body: body);
+
+        Console.WriteLine($" [*] Send {message}");
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
